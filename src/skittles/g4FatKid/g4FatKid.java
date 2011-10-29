c808ebf39ecdd6109cccab646cafc7e4fd6d42d8
Riddhi Mehta <rnmehta5@gmail.com>
2011-10-29 15:27:05 -0400

    our player p.s. i hope you like its name :)

---
+++ src/skittles/g4FatKid/g4FatKid.java
@@ -0,0 +1,70 @@
+package skittles.g4FatKid;
+
+import skittles.sim.*;
+
+public class g4FatKid extends Player{
+
+	@Override
+	public void eat(int[] aintTempEat) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void offer(Offer offTemp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void syncInHand(int[] aintInHand) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void happier(double dblHappinessUp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Offer pickOffer(Offer[] aoffCurrentOffers) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void offerExecuted(Offer offPicked) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateOfferExe(Offer[] aoffCurrentOffers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initialize(int intPlayerIndex, String strClassName,
+			int[] aintInHand) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getClassName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPlayerIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	//Todo: 
+	
+}
